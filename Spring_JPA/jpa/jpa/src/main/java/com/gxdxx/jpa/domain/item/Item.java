76c6c9d0f34e844b4557dd0f43b8c440864154e6
gxdxx <nkd0310@naver.com>
2022-01-26 14:38:07 +0900

    feat: 주문 도메인 개발

--- Spring_JPA/jpa/jpa/src/main/java/com/gxdxx/jpa/domain/item/Item.java
+++ Spring_JPA/jpa/jpa/src/main/java/com/gxdxx/jpa/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.gxdxx.jpa.domain.item;
 
 import com.gxdxx.jpa.domain.Category;
+import com.gxdxx.jpa.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 도메인 주도 설계를 할 때는 엔티티 자체에서 해결할 수 있는 것들은 엔티티 안에 비즈니스 로직을 넣는게 좋다.
+    //==비즈니스로직==//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity -= restStock;
+    }
 }
---
+++ Spring_JPA/jpa/jpa/src/test/java/com/gxdxx/jpa/service/ItemServiceTest.java
@@ -0,0 +1,54 @@
+package com.gxdxx.jpa.service;
+
+import com.gxdxx.jpa.domain.Member;
+import com.gxdxx.jpa.domain.item.Book;
+import com.gxdxx.jpa.domain.item.Item;
+import com.gxdxx.jpa.exception.NotEnoughStockException;
+import com.gxdxx.jpa.repository.ItemRepository;
+import com.gxdxx.jpa.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class ItemServiceTest {
+
+    @Autowired
+    ItemService itemService;
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    public void 상품등록() throws Exception {
+        //given
+        Book book = new Book();
+        book.setName("book");
+
+        //when
+        itemService.saveItem(book);
+
+        //then
+        assertEquals(book, itemRepository.findOne(book.getId()));
+    }
+
+    @Test(expected = NotEnoughStockException.class)
+    public void 상품_재고_예외() throws Exception {
+        //given
+        Book book = new Book();
+        book.setName("book");
+        book.setStockQuantity(100);
+
+        //when
+        book.removeStock(101);
+
+        //then
+        fail("예외가 발생해야 한다.");
+    }
+}
